fcde946399b5768bcc6333ef62013da413afaa21
markoaar <74166529+markoaar@users.noreply.github.com>
2022-11-30 19:33:35 -0500

    Script fixes

--- WWS_arenas/Assets/Scripts/p1_parabolic.cs
+++ WWS_arenas/Assets/Scripts/p1_parabolic.cs
@@ -1,25 +1,24 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class p1_parabolic : MonoBehaviour
+public class P1_parabolic : MonoBehaviour
 {
+    [Header("Parametros")]
     public float speed = 0;
-    public float gravity = 0;
     public float angle = 0;
+    public float gravity = 0;
+
     private float rad = 0;
     private float Vx = 0;
     private float Vy = 0;
-    Vector2 launchDir;
+    private Vector2 launchDir;
 
-    bool shoot = false;
+    private bool shoot = false;
 
     private void Update()
     {
 
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && shoot == false)
         {
-            ///move = Time.deltaTime * speed;
             Angle();
             shoot = true;
         }
@@ -30,17 +29,22 @@ public class p1_parabolic : MonoBehaviour
 
             transform.position += new Vector3(launchDir.x * Time.deltaTime, launchDir.y * Time.deltaTime);
         }
-    }   
+    }
+
+    private void OnBecameInvisible()
+    {
+        shoot = false;
+        transform.position = new Vector3(-5, 0);
+    }
 
     private void Angle()
     {
         rad = angle * Mathf.Deg2Rad;
-
         Vx = speed * Mathf.Cos(rad);
         Vy = speed * Mathf.Sin(rad);
 
         launchDir = new Vector2(Vx, Vy);
         float Degrees = Mathf.Atan2(launchDir.y, launchDir.x) * Mathf.Rad2Deg;
-        Debug.Log(Degrees);
+        Debug.Log(Degrees); //Se corrobora el angulo de lanzamiento
     }
 }
